6821fd2e8a17ec84ff9cb7b88a028ff993c39b00
einalem4 <melanie@codehangar.io>
2021-02-03 19:42:46 -0700

    set up  race function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,17 @@ var elem = document.querySelector('.collapsible.expandable');
 var instance = M.Collapsible.init(elem, {
   accordion: false
 });
+
+function searchRace() {
+    fetch('https://www.dnd5eapi.co/api/races')
+
+        .then(function (response) {
+            return response.json();
+        })
+
+        .then(function (response) {
+            console.log(response.results[0].name)
+
+        });
+}
+searchRace()
