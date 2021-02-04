5d50b82f32d9006efa437b54847199e3f96d1109
einalem4 <melanie@codehangar.io>
2021-02-04 09:41:48 -0700

    randomize race and class in the console

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,23 @@ var elem = document.querySelector('.collapsible.expandable');
 var instance = M.Collapsible.init(elem, { accordion: false });
 var generateCharacter = document.querySelector('#generate')
 
-function searchRace() {
+
+function getRandomRace() {
   fetch('https://www.dnd5eapi.co/api/races')
 
     .then(function (response) {
       return response.json();
     })
 
-    .then(function (race) {
-      console.log(race.results[0].name)
-
+    .then(function (races) {
+      var races = races.results[[Math.floor(Math.random() * races.results.length)]]
+      console.log(races.name)
+      return races.name
     });
 }
-searchRace()
+getRandomRace()
 
-function searchClass() {
+function getRandomClass() {
   fetch('https://www.dnd5eapi.co/api/classes')
 
     .then(function (response) {
@@ -24,16 +26,19 @@ function searchClass() {
     })
 
     .then(function (classes) {
-      console.log(classes.results[0].name)
+      var classes = classes.results[[Math.floor(Math.random() * classes.results.length)]]
+      console.log(classes.name)
+      return classes.name
 
     });
 }
-searchClass()
+getRandomClass()
 
 
 //when the generate character button is clicked it generates random information
 generateCharacter.addEventListener("click", function (e) {
   e.preventDefault();
-  searchRace();
+  getRandomRace();
+  getRandomClass();
   console.log("this worked!")
 });
