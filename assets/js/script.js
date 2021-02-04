206114f64a55053666f63642d593a35884790c1c
einalem4 <melanie@codehangar.io>
2021-02-03 19:53:04 -0700

    set up button click event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,25 @@
 var elem = document.querySelector('.collapsible.expandable');
-var instance = M.Collapsible.init(elem, {
-  accordion: false
-});
+var instance = M.Collapsible.init(elem, { accordion: false });
+var generateCharacter = document.querySelector('#generate')
 
 function searchRace() {
-    fetch('https://www.dnd5eapi.co/api/races')
+  fetch('https://www.dnd5eapi.co/api/races')
 
-        .then(function (response) {
-            return response.json();
-        })
+    .then(function (response) {
+      return response.json();
+    })
 
-        .then(function (response) {
-            console.log(response.results[0].name)
+    .then(function (response) {
+      console.log(response.results[0].name)
 
-        });
+    });
 }
 searchRace()
+
+
+//when the generate character button is clicked it generates random information
+generateCharacter.addEventListener("click", function (e) {
+  e.preventDefault();
+  searchRace();
+  console.log("this worked!")
+});
