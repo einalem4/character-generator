f4d3a977b21ddc59f8f993c16ccb8b85f6206089
thrazer675 <thrazer675@aol.com>
2021-02-05 20:39:13 -0500

    added save and load functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,18 @@ var elem = document.querySelector('.collapsible.expandable');
 var instance = M.Collapsible.init(elem, {
   accordion: false
 });
+
+const SAVED_CHARACTER_KEY = "savedCharacter"
+
+// function to save character to local storage
+function saveCharacter() {
+  var character = "sample";
+  localStorage.setItem(SAVED_CHARACTER_KEY, character);
+}
+
+function loadCharacter() {
+  var loadedCharacter = localStorage.getItem(SAVED_CHARACTER_KEY);
+}
+
+loadCharacter();
+document.getElementById("save-btn").addEventListener("click", saveCharacter);
